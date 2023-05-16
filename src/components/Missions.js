5f0904ce63109e561c8f694006f9ce67ed3f89ef
Adriano Costa <adrianofacosta@gmail.com>
2023-05-16 16:34:58 -0300

    :sparkles: :feat: add a className to Missions div

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -6,7 +6,7 @@ import MissionCard from './MissionCard';
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div data-testid="missions" className="missions-section">
         <Title headline="Missões" />
         {missions.map((mission) => (
           <MissionCard
